275f07b6355bdbb2aba17ccc94e223d737417779
tomek7777 <89088480+tomek7777@users.noreply.github.com>
2022-02-02 11:12:25 +0100

    Dodaj pliki projektów.

---
+++ 13556 egzamin/Models/Lista.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13556_egzamin.Models
+{
+    public class Lista
+    {
+        public int Id { get; set; }
+        [Display(Name = "Numer Indeksu")]
+        [Required(ErrorMessage = "Numer Indeksu is required")]
+        public int NrIndeksu { get; set; }
+
+        [Range(2, 5)]
+        [Display(Name = "Ocena")]
+        public int Ocena { get; set; }
+
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Przedmiot musi zawierać między 2, a 20 znaków")]
+        [Display(Name = "Przedmiot")]
+        public string Przedmiot { get; set; }
+
+    }
+}
